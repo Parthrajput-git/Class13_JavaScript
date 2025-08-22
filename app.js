7b701e974f39d6ebc7e7a4238bff43b11c61c658
Parthrajput-git <parthrajput3834@gmail.com>
2025-08-22 14:15:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// JavaScript Object
+let student={
+      student1: "Joy" ,
+      class: 12 ,
+      age : 20,
+      id: 12 ,
+      email: "joymari456@gmail.com"
+}
+console.log(student.student1);
+
+// function and object
+let employee={
+    name : "Vishan",
+    work: "Fullstack developer",
+    salary: function(){
+    console.log("My salary is 55000");
+    },
+    age: 21 
+}
+console.log(employee.name);
+console.log(employee.salary());
+
+//Third object
+let workars={
+    "Name he" : "Venu",
+    4356: "Id number",
+    salary : 30000,
+    experience : function(){
+          return console.log("5 year experience");
+    }
+}
+console.log(workars[4356]);
+console.log(workars.experience);
+
+// fourth object and object
+let office={
+    name : "grow",
+    work : "software development",
+    branch :{
+      name1 : "bet",
+      work: "help"
+    }
+}
+console.log(office.branch);
+
+//fifth object
+
+let valueRetrun={
+    first : "emty",
+    obj1 :{
+       value1:{
+        id: 100
+       }
+       
+    }
+}
+
+
+
+
+
+
+
